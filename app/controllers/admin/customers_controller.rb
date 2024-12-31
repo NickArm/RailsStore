21531603db379f5d1c27e09721f878213a3d0b16
Nick Armenis <armenisnick@gmail.com>
2024-12-31 14:02:31 +0200

    Added Customers Show Method @Admin Dashboard

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -3,5 +3,9 @@ module Admin
       def index
         @customers = Customer.all.order(created_at: :desc)
       end
+
+      def show
+        @customer = Customer.find(params[:id])
+      end
     end
 end
